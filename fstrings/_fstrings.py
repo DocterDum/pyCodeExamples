205c98403bc03b5e738255bb6fb5eba76cc5614f
Alec M <alec@mcgeary.com.au>
2022-06-16 13:19:21 +1000

    Better var names

--- fstrings/_fstrings.py
+++ fstrings/_fstrings.py
@@ -1,19 +1,19 @@
-string = 42
+integer = 42
 
 # :x -> hex
-print(f"{string:x}") # -> '2a'
+print(f"{integer:x}") # -> '2a'
 
 # :X -> hex (uppercase)
-print(f"{string:X}") # -> '2A'
+print(f"{integer:X}") # -> '2A'
 
 # :c -> ascii
-print(f"{string:c}") # -> '*'
+print(f"{integer:c}") # -> '*'
 
 # :o -> octal
-print(f"{string:o}") # -> '52'
+print(f"{integer:o}") # -> '52'
 
 # :b -> binary
-print(f"{string:b}") # -> '101010'
+print(f"{integer:b}") # -> '101010'
 
 # :010b -> combined with padding
-print(f"{string:010b}") # -> '0000101010'
+print(f"{integer:010b}") # -> '0000101010'
